dbc09b1ab626a7ac872e272dc023924c9a01f34e
Pratap29 <88335899+Pratap29@users.noreply.github.com>
2022-11-29 15:50:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php echo '<p>hello prathap you are as a cloudengineer, currently you are looking for . job, we are hiring a DEvOps Engineer<p>' ?>
+<?php echo '<p>hello prathap you are as a cloudengineer, currently you are looking for a job change . job, we are hiring a DevOps Engineer<p>' ?>
