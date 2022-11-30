1e4a9d52af5049ef2a588248de2b6dc3a42ce6a4
Pratap29 <88335899+Pratap29@users.noreply.github.com>
2022-11-30 15:31:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php  
-$name="Cat";  
-${$name}="Dog";  
-${${$name}}="Monkey";  
-echo $name. "<br>";  
-echo ${$name}. "<br>";  
-echo $Cat. "<br>";  
-echo ${${$name}}. "<br>";  
-echo $Dog. "<br>";  
-?>  
+ $x="U.P";  
+$$x="Lucknow";  
+echo $x. "<br>";  
+echo $$x. "<br>";  
+echo "Capital of $x is " . $$x;  
+?>   
