415ef81eaf617dab50fbe42ceef6ae2db5ece0f5
Pratap29 <88335899+Pratap29@users.noreply.github.com>
2022-12-02 18:21:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
-<div class=”background”>neato!</div> 
-<script> 
-if(<?php echo $pageType ?> === ‘admin’ ) { 
-document.querySelector(“.background”).addClass(“admin”); 
-}else{ 
-document.querySelector(“.background”).addClass(“user”); 
-} 
-</script> 
-<style> 
-.admin{ 
-background:green 
-} 
-.user{ 
-background:blue 
-} 
-</style> 
+<?php
+// https://coursesweb.net/php-mysql/
 
+// Create a 160x80 image
+$width = 160;
+$height = 80;
+$im = imagecreatetruecolor($width, $height);
+
+// sets a color for background
+$red = imagecolorallocate($im, 80, 120, 250);
+imagefill($im, 0, 0, $red);
+
+// sets and draw a green rectangle in left half
+$white = imagecolorallocate($im, 0, 245, 1);
+imagefilledrectangle($im, 0, 0, $width/2, $height, $white);
+
+// sets and adds a red text
+$text = 'CoursesWeb.net';
+$text_color = imagecolorallocate($im, 225, 0, 1);
+imagestring($im, 5, 12, $height/3, $text, $text_color);
+
+// Saves the image in 'imgs' folder
+imagepng($im, 'addons/php-mysql/image.png');
+?>
