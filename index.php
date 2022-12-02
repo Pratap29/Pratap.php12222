52a5c412a365dc50ed082a746865ecc9fc20ba5c
Pratap29 <88335899+Pratap29@users.noreply.github.com>
2022-12-02 17:28:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-body { 
-  background-color: yellow; 
-} 
+<?php
+$string= "Welcome! This page doesn't exist!";
+echo $string."<br>";
+
+//Change the fontsize using PHP, HTML and css through echo
+echo '<span style="font-size: 50px;"> ' . $string.  ', <a href="something.php">New Page</a></span>';
+
+//Change the font color using PHP, HTML and css through echo
+echo "<br>";
+echo '<span style="color: red; font-size: 50px;"> ' . $string.  ', <a href="something.php">New Page</a></span>';
+?>
 
 
