f10fd0a1b3e8b6a813b572b705ce4765fc50ae7e
Pratap29 <88335899+Pratap29@users.noreply.github.com>
2022-12-02 15:46:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
-<?php
-// Start the session
-session_start();
-?>
-<!DOCTYPE html>
-<html>
-<body>
+if blablabla {
 
-<?php
-// Set session variables
-$_SESSION["favcolor"] = "green";
-$_SESSION["favanimal"] = "cat";
-echo "Session variables are set.";
-?>
-
-</body>
-</html>
+ echo '<body style="background-color:white">';
+}
+else {
+ echo '<body style="background-color:orange">';
+}
