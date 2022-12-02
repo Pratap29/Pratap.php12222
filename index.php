2acd8550ee6cb92556f4168d855300a0b3f38aa4
Pratap29 <88335899+Pratap29@users.noreply.github.com>
2022-12-02 17:20:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,40 @@
 <html>
-<head><title>Front Door</title></head>
+<head>
+<title>Background Colors change based on the day of the week</title>
+</head>
 <?php
-//$bg = $_COOKIE['bg'];
-//$fg = $_COOKIE['fg'];
-$bg="black";
-$fg="red";
-?>
+//mistonline.in
+$today = date("l");
+print("$today");
+if($today == "Sunday")
+{
+$bgcolor = "#FEF0C5";
+}
+elseif($today == "Monday")
+{
+$bgcolor = "#FFFFFF";
+}
+elseif($today == "Tuesday")
+{
+$bgcolor = "#FBFFC4";
+}
+elseif($today == "Wednesday")
+{
+$bgcolor = "#FFE0DD";
+}
+elseif($today == "Thursday")
+{
+$bgcolor = "#E6EDFF";
+}
+elseif($today == "Friday")
+{
+$bgcolor = "#E9FFE6";
+}
+else
+{
+// Since it is not any of the days above it must be Saturday
+$bgcolor = "#F0F4F1";
+}
+print("<body bgcolor=\"$bgcolor\">\n");
 
-<body bgcolor="<?php=$bg ?>" text="<?php= $fg ?>">
-
-<h1>Prathap Reddy</h1>
-
-</body>
-</html>
 
