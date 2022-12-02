1421942fdd27c3050f4bc6b7c0bd8632d9f576d4
Pratap29 <88335899+Pratap29@users.noreply.github.com>
2022-12-02 12:32:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,26 +5,20 @@ $password = "password";
 $dbname = "phpmyadmin";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
 
-// sql to create table
-$sql = "CREATE TABLE PrathapReddy (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
 
-if (mysqli_query($conn, $sql)) {
-  echo "Table PrathapReddy created successfully";
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
 } else {
-  echo "Error creating table: " . mysqli_error($conn);
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-mysqli_close($conn);
+$conn->close();
 ?>
