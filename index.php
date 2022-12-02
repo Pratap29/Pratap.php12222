69a4acb4b16a5ba63360d39a6c69f611f5aeb257
Pratap29 <88335899+Pratap29@users.noreply.github.com>
2022-12-02 12:28:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (!$conn) {
 }
 
 // sql to create table
-$sql = "CREATE TABLE Prathap (
+$sql = "CREATE TABLE PrathapReddy (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 firstname VARCHAR(30) NOT NULL,
 lastname VARCHAR(30) NOT NULL,
@@ -21,7 +21,7 @@ reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 
 if (mysqli_query($conn, $sql)) {
-  echo "Table Prathap created successfully";
+  echo "Table PrathapReddy created successfully";
 } else {
   echo "Error creating table: " . mysqli_error($conn);
 }
