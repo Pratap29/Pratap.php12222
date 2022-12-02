90b9b70de446bf4c42307d65ac0cd0d08ade96e8
Pratap29 <88335899+Pratap29@users.noreply.github.com>
2022-12-02 16:47:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,99 +1,70 @@
 <?php
-include('dbconnect.php');
-
-if(isset($_POST['submit']))
+include ('dbconnect.php');
+if(isset($_POST['']))
 {
-$msg= user_signup($_POST);
+user_login($_POST);
 }
-
 ?>
 
 <html>
 <head> </head>
-
 <body>
-
 <section class="bg-grey">
-
 <div class="main">
 <div class="container">
 <div class="col-md-12">
 <div class="form-content-box">
 <div class="login-header">
-<h3 class="text-center"> Sign Up </h3>
+<h3 class="text-center"> Login </h3>
 </div>
-
-<div class="Details">
+<div class="details">
 <form action="" method="post">
 <div class="form-group">
-<input type="text" class="form-control" name="name" placeholder="Full Name" required>
+<input type="email" name="email" placeholder="Enter email" autocomplete="off" required 
+class="form-control" pattern="[a-z0-9._%+-]+@[a-z.-]+\.[a-z]{2,3}$">
+</div>
+<div class="form-group">
+<input type="password" name="password" class="form-control" placeholder="Password" required>
 </div>
 
 <div class="form-group">
-<input type="email" name="email" class="form-control" placeholder="Email Address" required>
+<button type="submit" name="login" class="btn btn-submit">login</button>
 </div>
-
-<div class="form-group">
-<input type="text" class="form-control"  name="phone" placeholder="Mobile" 
-pattern="(\+?\d[- .]*){10,10}" oninput="this.value = this.value.replace(/[^0-9.]/g, ''); this.value = 
-this.value.replace(/(\..*)\./g, '$1');" title="Please Enter Your 10 
-digit vaild number!" required>
-</div>
-
-<div class="form-group">
-<input type="password" name="password" class="form-control"  placeholder="Password" required>
-</div>
-
-<div class="form-group text-center">
-<button type="submit" name="submit" class="btn btn-submit">Signup</button>
-</div>
-
 </form>
 </div>
 
 <div class="login-footer">
-
 </div>
 </div>
 </div>
 </div>
 </div>
-
 <section>
-
 <div class="clearfix"></div>
-
 </body>
 </html>
 
 <?php
 
-function user_signup($data){
-
-$name = mysqli_real_escape_string($GLOBALS['db'],$data['name']);
-$email = mysqli_real_escape_string($GLOBALS['db'],$data['email']);
-$phone = mysqli_real_escape_string($GLOBALS['db'],$data['phone']);
-$pass = mysqli_real_escape_string($GLOBALS['db'],md5($data['password']));
-$qry=mysqli_query($GLOBALS['db'],"select * from users where (email='$email' || phone='$phone') ");
-
-if($qry->num_rows >0)
+function user_login($data){
+$email = $data['email'];
+$password = md5($data['password']);
+$login_qry = "select * from users where email='$email' AND password='$password'";
+$login=mysqli_query($GLOBALS['db'],$login_qry) or die ("mysql error2".mysqli_error($GLOBALS['db']));
+$result=mysqli_fetch_array($login);
+if(mysqli_num_rows($login)>0)
 {
-echo "<script>alert('Email Id or Phone Number already exists.')</script>";
-}
+$_SESSION['user']['id']=$result['id'];
 
+echo '<script type="text/javascript">window.location.href="home.php";</script>';
+
+}
 else
 {
-$sql="INSERT INTO users(name,email,phone,password) VALUES('$name','$email','$phone','$pass')";
-$query=mysqli_query($GLOBALS['db'],$sql) or die("Error 2".mysqli_error($GLOBALS['db']));
-if($query)
-{
-$sms_msg
- = "Thank You for registering with us.<br>Regards,<br>onTime
- Infotech,<br>https://www.ontimeinfotech.com/";
-echo "<script>alert('Congratulations!! Registration has been done successfully.')</script>";
-echo '<script>window.location.href="login.php";</script>';
+$_SESSION['user']['id']="";
+echo '<script type="text/javascript">alert("Please Enter Correct Username and Password");</script>';
 }
 }
-}
+
 ?>
 
