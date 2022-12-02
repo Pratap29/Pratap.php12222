e0a71935766aeacbc24bd4fe615df7c698d458f7
Pratap29 <88335899+Pratap29@users.noreply.github.com>
2022-12-02 16:44:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,84 +1,99 @@
-<!Doctype html>
-<html>
+<?php
+include('dbconnect.php');
 
-<head>
-<meta charset="utf-8">
-<title> Arithmetic Calculator </title>
-</head>
+if(isset($_POST['submit']))
+{
+$msg= user_signup($_POST);
+}
+
+?>
+
+<html>
+<head> </head>
 
 <body>
 
-<p align="center">
-Arithmetic Calculator (Plus / Minus / Multiply / Division)
-</p>
+<section class="bg-grey">
 
-<br>
-<?php
-@$first=$_POST['first'];
-@$second=$_POST['second'];
-if(isset($_POST['plus']))
-{
-$plus=$first+$second;
-echo '<table align="center">
-<tr>
-<td style="color:red;">Answer is: &nbsp;&nbsp;&nbsp;</td>
-<td><input type="text" value="'.$plus.'" readonly/></td>
-</tr>
-</table>';
-}
-else if(isset($_POST['minus']))
-{
-$minus=$first-$second;
-echo '<table align="center">
-<tr>
-<td style="color:red;">Answer is: &nbsp;&nbsp;&nbsp;</td>
-<td><input type="text" value="'.$minus.'" readonly/></td>
-</tr>
-</table>';
-}
-else if(isset($_POST['multiply']))
-{
-$multiply=$first*$second;
-echo '<table align="center">
-<tr>
-<td style="color:red;">Answer is: &nbsp;&nbsp;&nbsp;</td>
-<td><input type="text" value="'.$multiply.'" readonly/></td>
-</tr>
-</table>';
-}
-else if(isset($_POST['divide']))
-{
-$divide=$first/$second;
-echo '<table align="center">
-<tr>
-<td style="color:red;">Answer is: &nbsp;&nbsp;&nbsp;</td>
-<td><input type="text" value="'.$divide.'" readonly/></td>
-</tr>
-</table>';
-}
-?>
+<div class="main">
+<div class="container">
+<div class="col-md-12">
+<div class="form-content-box">
+<div class="login-header">
+<h3 class="text-center"> Sign Up </h3>
+</div>
 
+<div class="Details">
 <form action="" method="post">
-<table align="center">
-<tr>
-<td> First Value </td>
-<td><input type="text" name="first" /></td>
-</tr>
-<tr>
-<td> Second Value </td>
-<td><input type="text" name="second" /></td>
-</tr>
-</table>
-<table align="center">
-<tr>
-<td><input type="submit" name="plus" value="+"></td>
-<td><input type="submit" name="minus" value="-"></td>
-<td><input type="submit" name="multiply" value="*"></td>
-<td><input type="submit" name="divide" value="/"></td>
-</tr>
-</table>
+<div class="form-group">
+<input type="text" class="form-control" name="name" placeholder="Full Name" required>
+</div>
+
+<div class="form-group">
+<input type="email" name="email" class="form-control" placeholder="Email Address" required>
+</div>
+
+<div class="form-group">
+<input type="text" class="form-control"  name="phone" placeholder="Mobile" 
+pattern="(\+?\d[- .]*){10,10}" oninput="this.value = this.value.replace(/[^0-9.]/g, ''); this.value = 
+this.value.replace(/(\..*)\./g, '$1');" title="Please Enter Your 10 
+digit vaild number!" required>
+</div>
+
+<div class="form-group">
+<input type="password" name="password" class="form-control"  placeholder="Password" required>
+</div>
+
+<div class="form-group text-center">
+<button type="submit" name="submit" class="btn btn-submit">Signup</button>
+</div>
+
 </form>
+</div>
+
+<div class="login-footer">
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<section>
+
+<div class="clearfix"></div>
 
 </body>
 </html>
+
+<?php
+
+function user_signup($data){
+
+$name = mysqli_real_escape_string($GLOBALS['db'],$data['name']);
+$email = mysqli_real_escape_string($GLOBALS['db'],$data['email']);
+$phone = mysqli_real_escape_string($GLOBALS['db'],$data['phone']);
+$pass = mysqli_real_escape_string($GLOBALS['db'],md5($data['password']));
+$qry=mysqli_query($GLOBALS['db'],"select * from users where (email='$email' || phone='$phone') ");
+
+if($qry->num_rows >0)
+{
+echo "<script>alert('Email Id or Phone Number already exists.')</script>";
+}
+
+else
+{
+$sql="INSERT INTO users(name,email,phone,password) VALUES('$name','$email','$phone','$pass')";
+$query=mysqli_query($GLOBALS['db'],$sql) or die("Error 2".mysqli_error($GLOBALS['db']));
+if($query)
+{
+$sms_msg
+ = "Thank You for registering with us.<br>Regards,<br>onTime
+ Infotech,<br>https://www.ontimeinfotech.com/";
+echo "<script>alert('Congratulations!! Registration has been done successfully.')</script>";
+echo '<script>window.location.href="login.php";</script>';
+}
+}
+}
+?>
 
