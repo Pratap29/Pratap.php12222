24eb682c8295b484265f7507300a2957b6031199
Pratap29 <88335899+Pratap29@users.noreply.github.com>
2022-12-02 17:25:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,43 @@
-<html>
-<head>
-<title>Background Colors change based on the day of the week</title>
-</head>
 <?php
-//mistonline.in
-$today = date("l");
-print("$today");
-if($today == "Sunday")
+
+// A list of the days of the week. 
+$name_day = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+foreach ($name_day as $name);
+
 {
-$bgcolor = "#FEF0C5";
-}
-elseif($today == "Monday")
-{
-$bgcolor = "#FFFFFF";
-}
-elseif($today == "Tuesday")
-{
-$bgcolor = "#FBFFC4";
-}
-elseif($today == "Wednesday")
-{
-$bgcolor = "#FFE0DD";
-}
-elseif($today == "Friday")
-{
-$bgcolor = "#E6EDFF";
-}
-elseif($today == "Saturday")
-{
-$bgcolor = "#E9FFE6";
-}
-else
-{
-// Since it is not any of the days above it must be Saturday
-$bgcolor = "#F0F4F1";
-}
-print("<body bgcolor=\"$bgcolor\">\n");
+  if ($name == "Sunday") 
+      { // if day is Sunday background color is - Green.
+        $background_color = "Green";
+      }
+  elseif ($name == "Monday") 
+      {
+       $background_color = "#ff9966";
+      }
+  elseif ($name == "Tuesday")  
+      {
+       $background_color = "#cccc33";
+      }
+  elseif ($name == "Wednesday")  
+      {
+       $background_color = "#999900";
+      }
+  elseif ($name == "Thursday") 
+      {
+       $background_color = "#66cc33";
+      }
+  elseif ($name == "Friday") 
+      {
+       $background_color = "#cc0000";
+      }
+  elseif ($name == "Saturday")  
+      { // if day is not Sunday, but day is Saturday background color is - Purple.
+       $background_color = "ff66ff";
+      }
+}   
+
+$name_of = date("d-l-y"); // date, day, year.
+
+echo "$name_of";
 
 
