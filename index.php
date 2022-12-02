57ccaae791ed36b902626e8495314440f2d8c80d
Pratap29 <88335899+Pratap29@users.noreply.github.com>
2022-12-02 12:13:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,30 @@
 <?php
-$servername = "172.31.42.231";
-$username = "phpmyadmin";
+$servername = "43.205.233.97";
+$username = "root";
 $password = "password";
+$dbname = "phpmyadmin";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
 
-// Create database
-$sql = "CREATE DATABASE myDB";
-if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Table MyGuests created successfully";
 } else {
-  echo "Error creating database: " . $conn->error;
+  echo "Error creating table: " . mysqli_error($conn);
 }
 
-$conn->close();
+mysqli_close($conn);
 ?>
