cfdb57390dfe237c92c25fd05c193b0430799457
Pratap29 <88335899+Pratap29@users.noreply.github.com>
2022-12-02 18:17:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,17 @@
-<?php
-   if( $_POST["name"] || $_POST["age"] ) {
-      if (preg_match("/[^A-Za-z'-]/",$_POST['name'] )) {
-         die ("invalid name and name should be alpha");
-      }
-      
-      echo "Welcome ". $_POST['name']. "<br />";
-      echo "You are ". $_POST['age']. " years old.";
-      
-      exit();
-   }
-?>
-<html>
-   <body>
-   
-      <form action = "<?php $_PHP_SELF ?>" method = "POST">
-         Name: <input type = "text" name = "name" />
-         Age: <input type = "text" name = "age" />
-         <input type = "submit" />
-      </form>
-      
-   </body>
-</html>
+<div class=”background”>neato!</div> 
+<script> 
+if(<?php echo $pageType ?> === ‘admin’ ) { 
+document.querySelector(“.background”).addClass(“admin”); 
+}else{ 
+document.querySelector(“.background”).addClass(“user”); 
+} 
+</script> 
+<style> 
+.admin{ 
+background:green 
+} 
+.user{ 
+background:blue 
+} 
+</style> 
 
